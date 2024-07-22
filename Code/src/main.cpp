ea1b7d8d0a6e48553ca78d25da93a36138f8ab87
Pakin Sothisaovapark <pso28@uclive.ac.nz>
2024-07-22 14:47:03 +1200

    tof

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,47 +1,119 @@
-#include <Arduino.h>
-#include <QuadEncoder.h>
+/*
+This example shows how to set up and read multiple VL53L1X sensors connected to
+the same I2C bus. Each sensor needs to have its XSHUT pin connected to a
+different Arduino pin, and you should change sensorCount and the xshutPins array
+below to match your setup.
 
-uint32_t mCurPosValueL;
-uint32_t old_position = 0;
-uint32_t mCurPosValueR;
-uint32_t old_position1 = 0;
-QuadEncoder encLeft(1, 2, 3, 0);  // Encoder on channel 1 of 4 available
-                                 // Phase A (pin0), PhaseB(pin1), Pullups Req(0)
-QuadEncoder encRight(2, 4, 5, 0);  // Encoder on channel 2 of 4 available
-                                 //Phase A (pin2), PhaseB(pin3), Pullups Req(0)
-                                 
+For more information, see ST's application note AN4846 ("Using multiple VL53L0X
+in a single design"). The principles described there apply to the VL53L1X as
+well.
+*/
+
+#include <Wire.h>
+#include <VL53L0X.h>
+#include <VL53L1X.h>
+#include <SparkFunSX1509.h>
+
+const byte SX1509_ADDRESS = 0x3F;
+#define VL53L0X_ADDRESS_START 0x30
+#define VL53L1X_ADDRESS_START 0x35
+
+
+// The number of sensors in your system.
+const uint8_t sensorCount = 3;
+
+// The Arduino pin connected to the XSHUT pin of each sensor.
+const uint8_t xshutPinsL0[8] = {0,1};
+const uint8_t xshutPinsL1[8] = {2};
+
+SX1509 io; // Create an SX1509 object to be used throughout
+VL53L0X sensorsL0[sensorCount];
+VL53L1X sensorsL1[sensorCount];
+
 void setup()
 {
-  while(!Serial && millis() < 4000);
+  while (!Serial) {}
+  Serial.begin(115200);
 
-  /* Initialize the ENC module. */
-  QuadEncodersInit(encLeft, encRight);
+  io.begin(SX1509_ADDRESS);
+
+  Wire.begin();
+  Wire.setClock(400000); // use 400 kHz I2C
+
+  // Disable/reset all sensors by driving their XSHUT pins low.
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    io.pinMode(xshutPinsL0[i], OUTPUT);
+    io.digitalWrite(xshutPinsL0[i], LOW);
+    io.pinMode(xshutPinsL1[i], OUTPUT);
+    io.digitalWrite(xshutPinsL1[i], LOW);
+  }
+
+  // L0 Enable, initialize, and start each sensor, one by one.
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    // Stop driving this sensor's XSHUT low. This should allow the carrier
+    //.digitalWrite(xshutPinsL0[i], HIGH);
+    delay(10);
+
+    sensorsL0[i].setTimeout(500);
+    if (!sensorsL0[i].init())
+    {
+      Serial.print("Failed to detect and initialize sensor L0 ");
+      Serial.println(i);
+      while (1);
+    }
+
+    // Each sensor must have its address changed to a unique value other than
+    // the default of 0x29 (except for the last one, which could be left at
+    // the default). To make it simple, we'll just count up from 0x2A.
+    sensorsL0[i].setAddress(VL53L0X_ADDRESS_START + i);
+
+    sensorsL0[i].startContinuous(50);
+  }
+
+  // L1 Enable, initialize, and start each sensor, one by one.
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    // Stop driving this sensor's XSHUT low. This should allow the carrier
+    // board to pull it high. (We do NOT want to drive XSHUT high since it is
+    // not level shifted.) Then wait a bit for the sensor to start up.
+    //pinMode(xshutPins[i], INPUT);
+    io.digitalWrite(xshutPinsL1[i], HIGH);
+    delay(10);
+
+    sensorsL1[i].setTimeout(500);
+    if (!sensorsL1[i].init())
+    {
+      Serial.print("Failed to detect and initialize sensor L1 ");
+      Serial.println(i);
+      while (1);
+    }
+
+    // Each sensor must have its address changed to a unique value other than
+    // the default of 0x29 (except for the last one, which could be left at
+    // the default). To make it simple, we'll just count up from 0x2A.
+    sensorsL1[i].setAddress(VL53L1X_ADDRESS_START + i);
+
+    sensorsL1[i].startContinuous(50);
+  }
 }
 
-void loop(){
+void loop()
+{
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    Serial.print(sensorsL0[i].readRangeContinuousMillimeters());
+    if (sensorsL0[i].timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+    Serial.print('\t');
+  }
+
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    Serial.print(sensorsL1[i].read());
+    if (sensorsL1[i].timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+    Serial.print('\t');
+  }
   
-  /* This read operation would capture all the position counter to responding hold registers. */
-  mCurPosValueL = encLeft.read();
-
-  if(mCurPosValueL != old_position){
-    /* Read the position values. */
-    Serial.printf("Current position value1: %ld\r\n", mCurPosValueL);
-    Serial.printf("Position differential value1: %d\r\n", (int16_t)encLeft.getHoldDifference());
-    Serial.printf("Position HOLD revolution value1: %d\r\n", encLeft.getHoldRevolution());
-    Serial.println();
-  }
-
-  old_position = mCurPosValueL;
-
-  mCurPosValueR = encRight.read();
-
-  if(mCurPosValueR != old_position1){
-    /* Read the position values. */
-    Serial.printf("Current position value2: %ld\r\n", mCurPosValueR);
-    Serial.printf("Position differential value2: %d\r\n", (int16_t)encRight.getHoldDifference());
-    Serial.printf("Position revolution value2: %d\r\n", encRight.getHoldRevolution());
-    Serial.println();
-  }
-
-  old_position1 = mCurPosValueR;
+  Serial.println();
 }
