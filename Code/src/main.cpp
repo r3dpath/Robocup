e3e5fb636d6ebaf1e31394fd1b3e92342ee93508
r3dpath <137386245+r3dpath@users.noreply.github.com>
2024-08-05 12:33:36 +1200

    Changed main to TOF lib

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -17,6 +17,7 @@ well.
 #include <Arduino.h>
 #include <QuadEncoder.h>
 #include "BNO055_support.h"
+#include <TOF.h>
 
 const byte SX1509_ADDRESS = 0x3F;
 #define VL53L0X_ADDRESS_START 0x30
@@ -26,17 +27,7 @@ const byte SX1509_ADDRESS = 0x3F;
 #define MOVE_SPEED  350
 #define TURNING_RATIO  2/3
 
-// The number of sensors in your system.
-const uint8_t sensorCountL0 = 2;
-const uint8_t sensorCountL1 = 2;
-
-// The Arduino pin connected to the XSHUT pin of each sensor.
-const uint8_t xshutPinsL0[8] = {0,1};
-const uint8_t xshutPinsL1[8] = {2,3};
-
 SX1509 io; // Create an SX1509 object to be used throughout
-VL53L0X sensorsL0[sensorCountL0];
-VL53L1X sensorsL1[sensorCountL1];
 
 Servo myservoA,myservoB; //A left, B right
 
@@ -54,85 +45,35 @@ struct bno055_euler myEulerData; //Structure to hold the Euler data
 
 unsigned long lastTime = 0;
 
+TOF tof_l(L0, 0, 0x30, &io);
+TOF tof_r(L0, 1, 0x31, &io);
+TOF tof_b(L1, 2, 0x35, &io);
+TOF tof_f(L1, 3, 0x36, &io);
+
 void setup()
 {
-
   myservoA.attach(28);  
   myservoB.attach(29);  
   
   Serial.begin(115200);
+  Serial2.begin(19200);
 
   io.begin(SX1509_ADDRESS);
 
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
 
-  // Disable/reset all sensors by driving their XSHUT pins low.
-  for (uint8_t i = 0; i < sensorCountL0; i++)
-  {
-    io.pinMode(xshutPinsL0[i], OUTPUT);
-    io.digitalWrite(xshutPinsL0[i], LOW);
-  }
-
-  for (uint8_t i = 0; i < sensorCountL1; i++)
-  {
-    io.pinMode(xshutPinsL1[i], OUTPUT);
-    io.digitalWrite(xshutPinsL1[i], LOW);
-  }
-
-  // L0 Enable, initialize, and start each sensor, one by one.
-  for (uint8_t i = 0; i < sensorCountL0; i++)
-  {
-    // Stop driving this sensor's XSHUT low. This should allow the carrier
-    // board to pull it high. (We do NOT want to drive XSHUT high since it is
-    // not level shifted.) Then wait a bit for the sensor to start up.
-    //pinMode(xshutPins[i], INPUT);
-    io.digitalWrite(xshutPinsL0[i], HIGH);
-    delay(10);
-
-    sensorsL0[i].setTimeout(500);
-    if (!sensorsL0[i].init())
-    {
-      Serial.print("Failed to detect and initialize sensor L0 ");
-      Serial.println(i);
-      while (1);
-    }
-
-    // Each sensor must have its address changed to a unique value other than
-    // the default of 0x29 (except for the last one, which could be left at
-    // the default). To make it simple, we'll just count up from 0x2A.
-    sensorsL0[i].setAddress(VL53L0X_ADDRESS_START + i);
-
-    sensorsL0[i].startContinuous(50);
-  }
-
-  // L1 Enable, initialize, and start each sensor, one by one.
-  for (uint8_t i = 0; i < sensorCountL1; i++)
-  {
-    // Stop driving this sensor's XSHUT low. This should allow the carrier
-    // board to pull it high. (We do NOT want to drive XSHUT high since it is
-    // not level shifted.) Then wait a bit for the sensor to start up.
-    //pinMode(xshutPins[i], INPUT);
-    io.digitalWrite(xshutPinsL1[i], HIGH);
-    delay(10);
-
-    sensorsL1[i].setTimeout(500);
-    if (!sensorsL1[i].init())
-    {
-      Serial.print("Failed to detect and initialize sensor L1 ");
-      Serial.println(i);
-      while (1);
-    }
-
-    // Each sensor must have its address changed to a unique value other than
-    // the default of 0x29 (except for the last one, which could be left at
-    // the default). To make it simple, we'll just count up from 0x2A.
-    sensorsL1[i].setAddress(VL53L1X_ADDRESS_START + i);
-
-    sensorsL1[i].startContinuous(50);
-  }
   //set up encoder
   QuadEncodersInit(encLeft, encRight);
+
+  tof_f.init();
+  tof_b.init();
+  tof_l.init();
+  tof_r.init();
+  tof_f.startContinuous();
+  tof_b.startContinuous();
+  tof_l.startContinuous();
+  tof_r.startContinuous();
 
   //IMU setup
   //Initialize I2C communication
@@ -145,9 +86,6 @@ void setup()
   bno055_set_operation_mode(OPERATION_MODE_NDOF);
 
   delay(1);
-
-  //Initialize the Serial Port to view information on the Serial Monitor
-  Serial.begin(115200);
 }
 
 void Forward()
@@ -188,10 +126,10 @@ void mangItBackward()
 void MoveMent_Controller()
 {
   // Read sensor values
-  int sensorL1_1 = sensorsL1[0].read(); 
-  int sensorL1_2 = sensorsL1[1].read(); 
-  int sensorL0Distance1 = sensorsL0[0].readRangeContinuousMillimeters(); //Left side
-  int sensorL0Distance2 = sensorsL0[1].readRangeContinuousMillimeters(); //Right side
+  int sensorL1_1 = tof_b.read(); 
+  int sensorL1_2 = tof_f.read(); 
+  int sensorL0Distance1 = tof_l.read();  //Left side
+  int sensorL0Distance2 = tof_r.read();  //Right side
 
   static bool left = false, right = false; // Using static to retain state between function calls
 
