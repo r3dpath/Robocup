22a1882e2c0485a0356cdeee25bc0ed0cdcfc3fd
Brad Redpath <bre58@uclive.ac.nz>
2024-09-08 13:56:14 +1200

    Driving as well

---
+++ Code/lib/Movement/src/Movement.cpp
@@ -0,0 +1,55 @@
+#include "Movement.h"
+#include <Arduino.h>
+
+// Servo objects
+Servo myservoA, myservoB;
+
+void setupMovement() {
+    myservoA.attach(28);
+    myservoB.attach(29);
+}
+
+void Forward() {
+    myservoA.writeMicroseconds(1050);
+    myservoB.writeMicroseconds(1950);
+}
+
+void Backward() {
+    myservoA.writeMicroseconds(1950);
+    myservoB.writeMicroseconds(1050);
+}
+
+void RightTurn() {
+    myservoA.writeMicroseconds(1050);
+    myservoB.writeMicroseconds(1050);
+}
+
+void LeftTurn() {
+    myservoA.writeMicroseconds(1950);
+    myservoB.writeMicroseconds(1950);
+}
+
+void SlowBackward() {
+    myservoA.writeMicroseconds(1800);
+    myservoB.writeMicroseconds(1200);
+}
+
+void SlowForward() {
+    myservoA.writeMicroseconds(1300);
+    myservoB.writeMicroseconds(1700);
+}
+
+void mangItBackward() {
+    myservoA.writeMicroseconds(2000);
+    myservoB.writeMicroseconds(1000);
+}
+
+void smallLeft() {
+    myservoA.writeMicroseconds(1750);
+    myservoB.writeMicroseconds(1750);
+}
+
+void smallRight() {
+    myservoA.writeMicroseconds(1150);
+    myservoB.writeMicroseconds(1150);
+}
